b3a4a4c88d4f3c55570889f2773462753e70b18b
IrynaZazyba <lady.ira100@gmail.com>
2020-07-21 01:06:10 +0300

    bugfix

--- src/main/java/by/jwd/testsys/controller/parameter/SessionAttributeName.java
+++ src/main/java/by/jwd/testsys/controller/parameter/SessionAttributeName.java
@@ -9,7 +9,6 @@ public final class SessionAttributeName {
     public static final String USER_ROLE_SESSION_ATTRIBUTE = "user_role";
     public static final String LANGUAGE_ATTRIBUTE="local";
     public static final String QUERY_STRING="query_string";
-
     public static final String TYPE_ID="typeId";
     public static final String TEST_TITLE="testTitle";
     public static final String TEST_KEY="testKey";
